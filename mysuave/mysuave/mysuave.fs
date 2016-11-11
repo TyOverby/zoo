66ae629e576de9ff419bdbe3edf693b4fb7ca708
Ty Overby <ty@pre-alpha.com>
2016-11-11 10:01:02 -0800

    allow choosing a domain

--- mysuave/mysuave/mysuave.fs
+++ mysuave/mysuave/mysuave.fs
@@ -9,15 +9,15 @@ open FSharp.Data
 type RunsContext = JsonProvider<"./json-examples/jenkins/runslist.json">
 type BuildResultContext = JsonProvider<"./json-examples/jenkins/buildresult.json", SampleIsList=true>
 
-let getJenkins jobNames: WebPart =
+let getJenkins domain jobNames: WebPart =
     let fetchBuildResult path number = async {
-        let! buildResult =  BuildResultContext.AsyncLoad(sprintf "http://dotnet-ci2.cloudapp.net/%s/%d/api/json?pretty=true" path number)
+        let! buildResult =  BuildResultContext.AsyncLoad(sprintf "http://%s/%s/%d/api/json?pretty=true" domain path number)
         return sprintf "<li> %d: %s </li>" number buildResult.Result
     }
 
     fun (x : HttpContext) ->  async {
         let jobNesting = String.Join("/", jobNames |> Seq.collect (fun x -> ["job"; x]))
-        let! data = RunsContext.AsyncLoad(sprintf "http://dotnet-ci2.cloudapp.net/%s/api/json?pretty=true" jobNesting)
+        let! data = RunsContext.AsyncLoad(sprintf "http://%s/%s/api/json?pretty=true" domain jobNesting)
         let numbers = data.Builds |> Seq.map (fun b -> b.Number) |> Seq.take 10
         let! builds = numbers |> Seq.map (fetchBuildResult jobNesting) |> Async.Parallel
         let result = sprintf "<ul> %s </ul>" (String.Join("", builds))
@@ -28,7 +28,7 @@ let app =
     let splitPath (path: string) = path.Split('/') |> Seq.ofArray
     choose [
         GET >=> choose [
-            pathScan "/api/runs/%s" (splitPath >> getJenkins)
+            pathScan "/api/runs/%s/%s" (fun (domain, jobNames) -> getJenkins domain (splitPath jobNames))
         ]
     ]
 
