92347b6b4cfcb2e9ec2f9195404197b23d153865
Ty Overby <ty@pre-alpha.com>
2016-12-10 22:34:50 -0800

    zip up deploy zip

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+# Build
 (cd client && webpack)
 (cd server && bash build.sh)
 
 rm -rf out
+rm out.zip
+
+# Make out directory and move bits inside
 mkdir out
 cp -R client/dist ./out/static
 cp -R server/build ./out/server
 
+# Create a run.sh file
 (cat <<EOF
 mono ./server/Zoo.exe ./static
 EOF
 ) > out/run.sh
 
 chmod +x out/run.sh
+
+# Zip Folder creation
+zip -r out.zip out
