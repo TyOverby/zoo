7e8709eaf3a9eea5f5d51141db5aaec592d1af3b
Ty Overby <ty@pre-alpha.com>
2016-12-13 17:19:32 -0800

    An attempt was made

--- client/src/lens.ts
+++ client/src/lens.ts
@@ -1,28 +1,46 @@
-class ObjectLens<T extends Object> {
+class ObjectLens<T> {
     here: T
     constructor(v: T) {
         this.here = v;
     }
+
+    // down<I, K extends (keyof T) & I[]>(key: K): ArrayLens<I>;
+    down<K extends keyof T>(key: K): ObjectLens<T[K]>
+    {
+        return lens(this.here[key]);
+    }
+
+    which() {
+        console.log("obj");
+    }
 }
 
-class ArrayLens<I> {
-    here: I[]
+class ArrayLens<I, T extends I[]> extends ObjectLens<I[]> {
     constructor(v: I[]) {
-        this.here = v;
+        super(v)
+    }
+
+    which() {
+        console.log("array");
     }
 }
 
 function lens<T>(v: T): ObjectLens<T>;
-function lens<I, T extends I[]>(v: T): ArrayLens<I>
+function lens<I, T extends I[]>(v: T): ArrayLens<I, T>
 {
     if (Array.isArray(v)) { 
         return new ArrayLens(v);
-    } else if (typeof v == "object") {
+    } else {
         return new ObjectLens(v);
     }  
-    throw "unreachable";
 }
 
-let objlens: ObjectLens<{x: number}> = lens({x: 5});
-let arrlens: ArrayLens<number> = lens([4, 5]);
+let objlens: ObjectLens<{x: number, a: number[]}> = lens({x: 5, a: [1, 2, 3]});
+let arrlens: ArrayLens<number, number[]> = lens([4, 5]);
 let vlens: ObjectLens<number> = lens(5);
+
+let deepNumLens: ObjectLens<number> = objlens.down("x");
+let deepArrLens: ArrayLens<number, number[]> = objlens.down("a");
+
+deepNumLens.which();
+deepArrLens.which();
